c07d6b4e0b26fd7c363f1a158285c300d49fca86
K.A-Moloto <kgaugelob82@gmail.com>
2023-06-09 12:44:13 +0200

    pulled ex data

---
+++ Apex/Apex Basics & Database/force-app/main/default/triggers/ClosedOpportunityTrigger.trigger
@@ -0,0 +1,7 @@
+/**
+ * Created by kgaug on 2023/06/02.
+ */
+
+trigger ClosedOpportunityTrigger on Opportunity (after insert, after update) {
+
+}
--- webmobile/src/app/app.component.ts
+++ webmobile/src/app/app.component.ts
@@ -7,4 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'webmobile';
+  message = 'This is a sample website built with Angular!';
 }
